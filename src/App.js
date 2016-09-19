7d1e4d2a8bf862aa7dd65f2c2f0619b5b4468b22
Neel Bhat <neelbhat88@gmail.com>
2016-09-18 22:12:47 -0400

    Finished page 1 and skeleton of other 3 pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,163 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Pages />
     );
   }
 }
+
+var groomsman = {
+  "hershel bhat": {relation: "brother", years: 28},
+  "eric solomonson": {relation: "friend", years: 18},
+  "kevin adelman": {relation: "friend", years: 17},
+  "josh carleton": {relation: "friend", years: 10},
+  "jim kolodziej": {relation: "friend", years: 10},
+  "mark cassidy": {relation: "friend", years: 8},
+  "mark moschel": {relation: "friend", years: 8},
+  "husam najib": {relation: "friend", years: 10},
+  "siddharth bhalerao": {relation: "friend", years: 20},
+  "vikrant donthamsetti": {relation: "bil"},
+  "prashant donthamsetti": {relation: "bil"}
+}
+
+var Page1 = React.createClass({
+  getInitialState: function() {
+    return {
+      firstName: "",
+      lastName: "",
+      error: ""
+    };
+  },
+  submitName: function() {
+    this.setState({error: ""});
+
+    var firstName = this.state.firstName.trim();
+    var lastName = this.state.lastName.trim();
+    var fullName = firstName + " " + lastName
+    var groom = groomsman[fullName.toLowerCase()]
+
+    if (!firstName || !lastName || !groom) {
+      console.log("Groom does not exist");
+      this.setState({error: "Unable to find you in the CIA database. Please try using your real name."})
+      return;
+    }
+
+    this.setState({firstName: "", lastName: ""})
+
+    // Set state on parent
+    this.props.onGroomSelect(fullName.toLowerCase());
+    this.props.onChangeStep("next");
+  },
+  handleInput: function(e) {
+    var newState = {};
+    newState[e.target.id] = e.target.value;
+    this.setState(newState);
+  },
+  render: function() {
+    return (
+      <div>
+        <h1>Enter your name</h1>
+        <input type="text" placeholder="First name"
+          id="firstName" value={this.state.firstName}
+          onChange={this.handleInput} />
+        <input type="text" placeholder="Last name"
+          id="lastName" value={this.state.lastName}
+          onChange={this.handleInput} />
+        <button onClick={this.submitName}>Next</button>
+        <div className="error-text">{this.state.error}</div>
+      </div>
+    );
+  }
+});
+
+var Page2 = React.createClass({
+  nextStep: function() {
+    this.props.onChangeStep("next");
+  },
+  render: function() {
+    return (
+      <div>
+        <h1>Hi, {this.props.name}</h1>
+        <button onClick={this.props.onChangeStep.bind(null, "back")}>Back</button>
+        <button onClick={this.nextStep}>Next</button>
+      </div>
+    );
+  }
+});
+
+var Page3 = React.createClass({
+  render: function() {
+    return (
+      <h1>This is page 3</h1>
+    );
+  }
+});
+
+var Page4 = React.createClass({
+  render: function() {
+    return (
+      <h1>This is page 4</h1>
+    );
+  }
+});
+
+var Pages = React.createClass({
+  getInitialState: function() {
+    return {
+      step: 1,
+      name: ""
+    };
+  },
+  changeStep: function(action) {
+    switch (action) {
+      case "reset":
+        this.setState({step: 1});        
+        break;
+      case "back":
+        this.setState({step: this.state.step - 1})
+        break;
+      case "next":
+        this.setState({step: this.state.step + 1})
+        break;
+      default:
+    }
+  },
+  selectGroom: function(name) {
+    this.setState({name: name});
+  },
+  render: function() {
+    var page = null;
+
+    var defaultPage = <Page1 onChangeStep={this.changeStep} onGroomSelect={this.selectGroom} />;
+    switch (this.state.step) {
+      case 1:
+        page = defaultPage
+        break;
+      case 2:
+        page = <Page2 name={this.state.name} onChangeStep={this.changeStep} />;
+        break;
+      case 3:
+        page = <Page3 name={this.state.name} onChangeStep={this.changeStep} />;
+        break;
+      case 4:
+        page = <Page4 name={this.state.name} onChangeStep={this.changeStep} />;
+        break;
+      default:
+        page = defaultPage
+    }
+
+    return (
+      <div>
+        <div>{page}</div>
+        <div className="button-container">
+          <button onClick={() => this.changeStep("reset")}>Reset</button>
+        </div>
+      </div>
+    );
+  }
+});
 
 export default App;

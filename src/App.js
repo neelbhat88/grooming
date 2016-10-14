881da5ea91d7f6490e1fb27933cb0b0bb4a06be7
Neel Bhat <neelbhat88@gmail.com>
2016-10-13 23:29:33 -0400

    Design of Step 1 complete

--- src/App.js
+++ src/App.js
@@ -10,17 +10,28 @@ class App extends Component {
 }
 
 var groomsman = {
-  "hershel bhat": {relation: "brother", years: 28},
-  "eric solomonson": {relation: "friend", years: 18},
-  "kevin adelman": {relation: "friend", years: 17},
-  "josh carleton": {relation: "friend", years: 10},
-  "jim kolodziej": {relation: "friend", years: 10},
-  "mark cassidy": {relation: "friend", years: 8},
-  "mark moschel": {relation: "friend", years: 8},
-  "husam najib": {relation: "friend", years: 10},
-  "siddharth bhalerao": {relation: "friend", years: 20},
-  "vikrant donthamsetti": {relation: "bil"},
-  "prashant donthamsetti": {relation: "bil"}
+  "hershel bhat":
+    {relation: "brother", years: 28, nickname: "Hershwiz", },
+  "eric solomonson":
+    {relation: "friend", years: 18, nickname: "Eric"},
+  "kevin adelman":
+    {relation: "friend", years: 17, nickname: "Kev"},
+  "josh carleton":
+    {relation: "friend", years: 10, nickname: "Joshua"},
+  "jim kolodziej":
+    {relation: "friend", years: 10, nickname: "Jimmy"},
+  "mark cassidy":
+    {relation: "friend", years: 8, nickname: "Mark"},
+  "mark moschel":
+    {relation: "friend", years: 8, nickname: "Mark"},
+  "husam najib":
+    {relation: "friend", years: 10, nickname: "Tom Petty"},
+  "siddharth bhalerao":
+    {relation: "friend", years: 20, nickname: "Sid"},
+  "vikrant donthamsetti":
+    {relation: "bil", nickname: "Veeeeeeeeek"},
+  "prashant donthamsetti":
+    {relation: "bil", nickname: "Prashant"}
 }
 
 var Page1 = React.createClass({
@@ -40,7 +51,6 @@ var Page1 = React.createClass({
     var groom = groomsman[fullName.toLowerCase()]
 
     if (!firstName || !lastName || !groom) {
-      console.log("Groom does not exist");
       this.setState({error: "Unable to find you in the CIA database. Please try using your real name."})
       return;
     }
@@ -58,16 +68,27 @@ var Page1 = React.createClass({
   },
   render: function() {
     return (
-      <div>
-        <h1>Enter your name</h1>
-        <input type="text" placeholder="First name"
-          id="firstName" value={this.state.firstName}
-          onChange={this.handleInput} />
-        <input type="text" placeholder="Last name"
-          id="lastName" value={this.state.lastName}
-          onChange={this.handleInput} />
-        <button onClick={this.submitName}>Next</button>
-        <div className="error-text">{this.state.error}</div>
+      <div className="page1 container">
+        <div className="row">
+          <div className="col-xs-12 text-center">
+            <h1>Enter your name</h1>
+            <div className="input-container">
+              <input type="text" placeholder="First name"
+                id="firstName" value={this.state.firstName}
+                onChange={this.handleInput}
+                className="name-input" />
+              <input type="text" placeholder="Last name"
+                id="lastName" value={this.state.lastName}
+                onChange={this.handleInput}
+                className="name-input" />
+            </div>
+            <div className="button-container">
+              <button onClick={this.submitName}
+                className="btn btn-primary">Next</button>
+              <div className="error-text">{this.state.error}</div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
@@ -80,7 +101,10 @@ var Page2 = React.createClass({
   render: function() {
     return (
       <div>
-        <h1>Hi, {this.props.name}</h1>
+        <h1>Hey, {this.props.groomData.nickname}</h1>
+        <h3>We've known each other now for {this.props.groomData.years} years! We're getting old ;-)</h3>
+        <h5>pictures here...</h5>
+        <h3>Next year, on July 22 2017, I'm taking the next big step in life. So I have a question for you...</h3>
         <button onClick={this.props.onChangeStep.bind(null, "back")}>Back</button>
         <button onClick={this.nextStep}>Next</button>
       </div>
@@ -91,15 +115,60 @@ var Page2 = React.createClass({
 var Page3 = React.createClass({
   render: function() {
     return (
-      <h1>This is page 3</h1>
+      <div>
+        <h3>Since I'm currently living here</h3>
+        <h5>Map of Lexington</h5>
+        <h3>And you are living here</h3>
+        <h5>Map of their location</h5>
+        <h3>We'll have to use our imaginations a bit</h3>
+        <button onClick={this.props.onChangeStep.bind(null, "back")}>Back</button>
+        <button onClick={this.props.onChangeStep.bind(null, "next")}>Next</button>
+      </div>
     );
   }
 });
 
 var Page4 = React.createClass({
+  answerNo: function() {
+    alert("Congrats! You have just been signed up for hourly texts from Cat Facts - the leading provider of useless information about Cats. This service cannot be cancelled. Enjoy!");
+  },
   render: function() {
     return (
-      <h1>This is page 4</h1>
+      <div>
+        <h3>So let's pretend, we're hanging out. Maybe at a bar</h3>
+        <h5>picture of a bar</h5>
+        <h3>With some sort of drink in hand</h3>
+        <h5>picture with a drink in hand</h5>
+        <h5>question</h5>
+        <button onClick={this.answerNo}>No</button>
+        <button onClick={this.props.onChangeStep.bind(null, "next")}>YES!</button>
+
+        <button onClick={this.props.onChangeStep.bind(null, "back")}>Back</button>
+      </div>
+    );
+  }
+});
+
+var Page5 = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h5>Gif here: "http://i.giphy.com/3ornk5Sou1XMaL44bS.gif"</h5>
+
+        <button onClick={this.props.onChangeStep.bind(null, "back")}>Back</button>
+        <button onClick={this.props.onChangeStep.bind(null, "next")}>Next</button>
+      </div>
+    );
+  }
+});
+
+var Page6 = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h3>{this.props.groomData.nickname}, looking forward to having you be a part of the big day!</h3>
+        <button onClick={this.props.onChangeStep.bind(null, "back")}>Back</button>
+      </div>
     );
   }
 });
@@ -108,13 +177,14 @@ var Pages = React.createClass({
   getInitialState: function() {
     return {
       step: 1,
-      name: ""
+      name: "",
+      groomData: {}
     };
   },
   changeStep: function(action) {
     switch (action) {
       case "reset":
-        this.setState({step: 1});        
+        this.setState({step: 1});
         break;
       case "back":
         this.setState({step: this.state.step - 1})
@@ -126,7 +196,7 @@ var Pages = React.createClass({
     }
   },
   selectGroom: function(name) {
-    this.setState({name: name});
+    this.setState({name: name, groomData: groomsman[name]});
   },
   render: function() {
     var page = null;
@@ -137,13 +207,19 @@ var Pages = React.createClass({
         page = defaultPage
         break;
       case 2:
-        page = <Page2 name={this.state.name} onChangeStep={this.changeStep} />;
+        page = <Page2 name={this.state.name} groomData={this.state.groomData} onChangeStep={this.changeStep} />;
         break;
       case 3:
-        page = <Page3 name={this.state.name} onChangeStep={this.changeStep} />;
+        page = <Page3 name={this.state.name} groomData={this.state.groomData} onChangeStep={this.changeStep} />;
         break;
       case 4:
-        page = <Page4 name={this.state.name} onChangeStep={this.changeStep} />;
+        page = <Page4 name={this.state.name} groomData={this.state.groomData} onChangeStep={this.changeStep} />;
+        break;
+      case 5:
+        page = <Page5 onChangeStep={this.changeStep} />;
+        break;
+      case 6:
+        page = <Page6 name={this.state.name} groomData={this.state.groomData} onChangeStep={this.changeStep} />;
         break;
       default:
         page = defaultPage
@@ -151,10 +227,12 @@ var Pages = React.createClass({
 
     return (
       <div>
-        <div>{page}</div>
-        <div className="button-container">
-          <button onClick={() => this.changeStep("reset")}>Reset</button>
+        <div className="reset-container">
+          <i onClick={() => this.changeStep("reset")}
+             className="fa fa-sign-out clickable"
+             title="Reset"></i>
         </div>
+        <div>{page}</div>
       </div>
     );
   }
